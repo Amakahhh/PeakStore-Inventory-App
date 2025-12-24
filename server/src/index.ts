878c9626fac4bc9e368841ae64839c7f341998a7
Amaka <cejike.2202794@stu.edu.ng>
2025-12-24 16:36:36 +0100

    additions

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,6 +15,12 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Request logging middleware
+app.use((req, res, next) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+    next();
+});
+
 app.get('/', (req, res) => {
   res.send('Shop Management API is running');
 });
